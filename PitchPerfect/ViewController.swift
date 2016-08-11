7e0316cfe8770509247b6766cfff323bb690c8cd
theMonarK <anthony_guillier@live.fr>
2016-08-11 02:30:39 +0200

    ViewController & Multiple Views. Auto layouts & Buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        recordingLabel.text = "Recording in progress..."
+        stopRecordingButton.enabled = true
+        recordButton.enabled = false
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        recordingLabel.text = "Tap to record !"
+        stopRecordingButton.enabled = false
+        recordButton.enabled = true
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+    }
 }
 
